31914e351fdefec28af956be8aeaddcc9ce795a0
PastLeo <chgu82837@gmail.com>
2018-07-06 00:18:20 +0800

    ch12: supervisor

--- dic/lib/dic/application.ex
+++ dic/lib/dic/application.ex
@@ -1,7 +1,17 @@
 defmodule Dic.Application do
   use Application
+  import Supervisor.Spec
 
   def start(_type, _args) do
-    Dic.start()
+    children = [
+      worker(Dic.WordList, [])
+    ]
+
+    options = [
+      name: Dic.Supervisor,
+      strategy: :one_for_one,
+    ]
+
+    Supervisor.start_link(children, options)
   end
 end
