f5e973c49f224ca6716c78b5be113d6ca0495ecb
PastLeo <chgu82837@gmail.com>
2018-08-20 00:58:38 +0800

    ch16: complete dist text client

--- text_client/lib/text_client/interact.ex
+++ text_client/lib/text_client/interact.ex
@@ -1,8 +1,9 @@
 defmodule TextClient.Interact do
   alias TextClient.{State, Player}
+  @hangman_server :hangman@pc
 
   def start() do
-    Hangman.new_game()
+    new_game()
     |> init_state()
     |> play()
   end
@@ -17,4 +18,25 @@ defmodule TextClient.Interact do
   defp play(game) do
     Player.play(game)
   end
+
+  defp new_game() do
+    Node.connect(@hangman_server)
+    :rpc.call(@hangman_server, Hangman, :new_game, [])
+  end
 end
+
+# cd hangman
+# iex --sname hangman -S mix
+#
+# hangman > :observer.start
+# check observer application tab
+#
+# cd text_client
+# iex --sname c1 -S mix
+# iex --sname c2 -S mix
+#
+# c1 > TextClient.start
+# c1 > play hangman...
+#
+# c2 > TextClient.start
+# c2 > play hangman...
